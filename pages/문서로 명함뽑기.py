0d891bb5c891f32f392c8e32321462ae421f9e81
jasonheesanglee <volvstang@gmail.com>
2024-05-21 00:23:01 +0900

    updating file

--- pages/문서로 명함뽑기.py
+++ pages/문서로 명함뽑기.py
@@ -4,7 +4,7 @@ st.set_page_config('자동 명함뽑기', page_icon='💵')
 import pandas as pd
 from BBobgi import BBobgi
 
-bbobgi = BBobgi(openai_api_key)
+bbobgi = BBobgi()
 
 def df_col_list(file_, df):
     col_name = st.sidebar.text_input(
